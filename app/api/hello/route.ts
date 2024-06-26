015743c557d35121da2c8960c9119d5da5153fed
liudengfeng <liudengfeng_sd@outlook.com>
2024-06-27 03:51:25 +0800

    KV

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,10 @@
-import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
+import type { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  let responseText = 'Hello World'
+  let responseText = "Hello World";
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
@@ -13,10 +13,13 @@ export async function GET(request: NextRequest) {
   // )
   //
   // KV Example:
-  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
-
-  return new Response(responseText)
+  const myKv = getRequestContext().env.EN_WORDS;
+  // const key = `(Good) morning`;
+  const key = `ANC`;
+  const kvValueString = await myKv.get(key);
+  console.log(kvValueString);
+  const kvValue = kvValueString ? JSON.parse(kvValueString) : {};
+  return new Response(
+    `The value of kvTest in ${key} is: ${JSON.stringify(kvValue)}`
+  );
 }
